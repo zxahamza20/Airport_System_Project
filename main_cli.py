abda0a295332e31832ef2405f1f631da423199cb
Jaafar <jaafarasfahani8@gmail.com>
2026-04-30 18:52:45 -0500

    Update main_cli.py

--- main_cli.py
+++ main_cli.py
@@ -15,22 +15,24 @@ while True:
     cmd = prompt.split('(')[0]
     args = re.findall(r'"(.*?)"', prompt)
     
-    match cmd:
-        case "report":
-            startDate = args[0] if len(args) > 0 else '2000-01-01'
-            endDate = args[1] if len(args) > 1 else '2030-01-01'
-            startTime = args[2] if len(args) > 2 else '00:00:00'
-            endTime = args[3] if len(args) > 3 else '23:59:59'
-            
-            report(cursor, startDate, endDate, startTime, endTime)
-        
-         case "passenger":
+match cmd:
+    case "report":
+        startDate = args[0] if len(args) > 0 else '2000-01-01'
+        endDate = args[1] if len(args) > 1 else '2030-01-01'
+        startTime = args[2] if len(args) > 2 else '00:00:00'
+        endTime = args[3] if len(args) > 3 else '23:59:59'
+
+        report(cursor, startDate, endDate, startTime, endTime)
+
+    case "passenger":
         name = args[0] if len(args) > 0 else None
+
         if name:
             passenger_itinerary(name)
         else:
-            print('Usage: passenger("Customer Name")') 
-        case "exit":
-            break
+            print('Usage: passenger("Customer Name")')
+
+    case "exit":
+        break
 
 close_connection(conn)
